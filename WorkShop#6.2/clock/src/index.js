9040ef968cffc96289f0e330c1bac397e575b732
Jean Carlo Peralta <58532380+JeanCarloPeralta@users.noreply.github.com>
2022-07-02 02:43:30 -0600

    Add files via upload

---
+++ WorkShop#6.2/clock/src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM, { createRoot } from 'react-dom/client';
+import './index.css';
+import Controls from './components/Controls';
+
+
+const root = ReactDOM.createRoot(createRoot(document.getElementById('root')));
+root.render(<Controls/>);
+
+
