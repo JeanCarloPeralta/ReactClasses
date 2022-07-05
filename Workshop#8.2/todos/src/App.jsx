647e878504f57cad24d5c729216a50dce430b5d2
Jean Carlo Peralta <58532380+JeanCarloPeralta@users.noreply.github.com>
2022-07-05 02:15:10 -0600

    Add files via upload

---
+++ Workshop#8.2/todos/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import TodoList from './todos/TodoList';
+
+const App = function(){
+    return(
+    <div className="App">
+        <TodoList/>
+    </div>
+
+    )
+}
+
+export default App;
---
+++ Workshop#8.2/todos/src/todos/TodoItem.jsx
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import './TodoItem.css';
+
+const TodoItem = function ({todo, onRemovePressed, onCompletedPressed}) {
+  return (
+    <div className="todo-item-container">
+      <h3 className={todo.isCompleted? "completed" : ""}>{todo.text}</h3>
+      <div className="buttons-container">
+        <button
+          className="completed-button"
+          onClick={() => onCompletedPressed(todo.text)}
+        >Mark as completed</button>
+        <button
+          className="remove-button"
+          onClick={() => onRemovePressed(todo.text)}
+        >Remove</button>
+      </div>
+    </div>
+  );
+};
+
+export default TodoItem;
